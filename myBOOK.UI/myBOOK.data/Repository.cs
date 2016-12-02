a55dde6bb8d00e682e1408082a16fc97fe4bcf39
Alina Velnikovskaya <velnikovskaya@icloud.com>
2016-12-02 22:00:10 +0300

    Added new 5 requests.

--- myBOOK.UI/myBOOK.data/Repository.cs
+++ myBOOK.UI/myBOOK.data/Repository.cs
@@ -43,6 +43,71 @@ namespace myBOOK.data
             }
         }
 
+        public List<Favourite> ChooseUsersFavouriteBooks (string login)
+        {
+
+            using (Context c = new Context())
+            {
+                var result = (from s in c._Favourite
+                              where s.User.Login == login
+                              select s).ToList();
+                return result;
+            }
+
+        }
+
+        public List<FutureReadBooks> ChooseUsersFutureBooks(string login)
+        {
+
+            using (Context c = new Context())
+            {
+                var result = (from s in c._FutureReadBooks
+                              where s.User.Login == login
+                              select s).ToList();
+                return result;
+            }
+
+        }
+
+        public List<PastReadBooks> ChooseUsersPastBooks(string login)
+        {
+
+            using (Context c = new Context())
+            {
+                var result = (from s in c._PastReadBooks
+                              where s.User.Login == login
+                              select s).ToList();
+                return result;
+            }
+
+        }
+
+        public List<Reviews> ChooseReviewForABook(string BookName)
+        {
+
+            using (Context c = new Context())
+            {
+                var result = (from s in c._Review
+                              where s.Book.BookName == BookName
+                              select s).ToList();
+                return result;
+            }
+
+        }
+
+        public double ViewRatingForABook(string BookName)
+        {
+
+            using (Context c = new Context())
+            {
+             var result= (from s in c._Score
+                              where s.Book.BookName == BookName
+                              select s.Value).Average();
+                return result;
+            }
+
+        }
+
 
 
     }
