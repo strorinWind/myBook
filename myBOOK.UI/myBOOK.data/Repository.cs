f7e4635c2636b21749a05b394f485a835813286a
Alina Velnikovskaya <velnikovskaya@icloud.com>
2016-12-02 21:01:23 +0300

    Alina added two requests.

--- myBOOK.UI/myBOOK.data/Repository.cs
+++ myBOOK.UI/myBOOK.data/Repository.cs
@@ -8,10 +8,43 @@ namespace myBOOK.data
 {
     public class Repository
     {
-        public bool IsUserDataCorrect(string username,string password)
+        public bool IsUserDataCorrect(string login, string password)
         {
-            //if
-            return true; 
+            using (Context c = new Context())
+            {
+                var result = (from s in c._User
+                              where s.Login == login
+                              where s.Password == password
+                              select s).ToList();
+
+                if (result != null)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+
         }
+
+        public bool IsLoginRepeated (string login)
+        {
+            using (Context c = new Context())
+            {
+                var result = (from s in c._User
+                              where s.Login == login
+                              select s).ToList();
+
+                if (result != null)
+                {
+                    return false;
+                }
+                else
+                    return true;
+            }
+        }
+
+
+
     }
 }
