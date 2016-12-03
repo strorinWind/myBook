854d482f5411df021686790c53f997bab028c216
StrorinWinder <StrorinWinder@DESKTOP-VDRSH12>
2016-12-03 14:41:59 +0300

    Structure + added Encription class

---
+++ myBOOK.UI/myBOOK.data/Encryption.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myBOOK.data
+{
+    public class Encryption
+    {
+        //ecryption method line 1-22 from http://skillcoding.com/Default.aspx?id=165
+        public static string GetHashString(string s)
+        {
+            //переводим строку в байт-массим  
+            byte[] bytes = Encoding.Unicode.GetBytes(s);
+
+            //создаем объект для получения средст шифрования  
+            MD5CryptoServiceProvider CSP = new MD5CryptoServiceProvider();
+
+            //вычисляем хеш-представление в байтах  
+            byte[] byteHash = CSP.ComputeHash(bytes);
+
+            string hash = string.Empty;
+
+            //формируем одну цельную строку из массива  
+            foreach (byte b in byteHash)
+                hash += string.Format("{0:x2}", b);
+
+            return hash;
+        }
+    }
+}
