33006cafeb365269a1c438725d6c5e80e2006c5f
StrorinWinder <StrorinWinder@DESKTOP-VDRSH12>
2016-12-06 18:43:45 +0300

    Tabcontrol om profile

--- myBOOK.UI/myBOOK.UI/Profile.xaml.cs
+++ myBOOK.UI/myBOOK.UI/Profile.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using myBOOK.data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,11 @@ namespace myBOOK.UI
         public Profile(int id)
         {
             InitializeComponent();
+            using (Context c = new Context())
+            {
+                PastBookList.ItemsSource = c._Book.ToList();
+                FutureBookList.ItemsSource = c._Book.ToList();
+            }
         }
     }
 }
