d3c7271376c9c555ca22694df0f23808ec21fdb9
Nati <natuska97@yandex.ru>
2016-12-17 23:21:37 +0300

    Updated Profile

--- myBOOK.UI/myBOOK.UI/Profile.xaml.cs
+++ myBOOK.UI/myBOOK.UI/Profile.xaml.cs
@@ -242,5 +242,30 @@ namespace myBOOK.UI
             var b = converter.ConvertToBook((BookView)RecomendationList.SelectedItem);
             ShowBookInfo(b);
         }
+        private void MenuHelp_Click(object sender, RoutedEventArgs e)
+        {
+            Help help = new Help();
+            help.ShowDialog();
+        }
+        private void MenuClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuAbout_Click(object sender, RoutedEventArgs e)
+        {
+            About about = new About();
+            about.ShowDialog();
+        }
+        private void MenuAuthors_Click(object sender, RoutedEventArgs e)
+        {
+            Authors authors = new Authors();
+            authors.ShowDialog();
+        }
+
+        private void tabcontrol_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
